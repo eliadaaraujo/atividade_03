143eb7b52c0fb0f70c0c510d2b3b5e954c139521
eli <eliadadearaujolinhares@gmail.com>
2025-09-24 19:54:31 -0300

    Add files via upload

---
+++ atividade03.py
@@ -0,0 +1,17 @@
+# Atividade 43– Tipos de Dados
+
+# Crie 4 variáveis:
+# Um número inteiro
+# Um número decimal
+# Um texto
+# Um valor lógico (True ou False)
+
+# Imprima todas elas no console.
+
+numero_inteiro = 40
+numero_decimal = 40.98
+texto = "abacate é bom"
+valor_logico = True
+
+print(f'Numero inteiro: {numero_inteiro}, numero decimal: {numero_decimal}, texto: {texto}, valor_logico: {valor_logico}')
+
